4129de3d306f5ffca773caa607e082b10d57ca50
chebli97 <saif.chebli.gm13@gmail.com>
2024-11-16 22:30:52 +0100

    to do list app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState , useRef } from 'react'
 
 function App() {
+
+  const [todos , setTodos] = useState([])
+
+  const inputRef = useRef()
+
+  const handleAddTodo = () => {
+    const text = inputRef.current.value
+    const newItem = {completed : false , text}
+    if(text !== ''){
+      setTodos([...todos,newItem])
+      inputRef.current.value = ''
+    }
+  }
+
+  const handleItemDone = (index) => {
+    const newTodos = [...todos]
+    newTodos[index].completed = !newTodos[index].completed
+    setTodos(newTodos)
+  }
+
+  const handleDeleteItem = (index) => {
+    const newTodos = [...todos]
+    newTodos.splice(index , 1)
+    setTodos(newTodos)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h2>TO DO List</h2>
+        <div className="to-do-container">
+            <ul>
+              {
+                todos.map(({text , completed} , index) => {
+                  return (
+                    <div className='item'>
+                      <li 
+                      onClick={() => handleItemDone(index)}
+                      className={completed ? 'done' : ''}
+                      >{text}</li>
+
+                      <span className='delete-icon' onClick={() => handleDeleteItem(index)}>❌</span> 
+                    </div>
+                  )
+                })
+              }
+            </ul>
+            <input ref={inputRef} placeholder='Enter Item...' autoFocus />
+            <button onClick={handleAddTodo}>Add</button>
+        </div>
     </div>
   );
 }
